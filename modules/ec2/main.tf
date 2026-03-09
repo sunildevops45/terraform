24768d6d1109d69fbfd9f2f1f62fb9c0ff094653
sunildevops45 <sunildevops45@gmail.com>
2026-03-09 18:08:16 +0530

    Add files via upload

---
+++ modules/ec2/main.tf
@@ -0,0 +1,20 @@
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,6 @@
+
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
